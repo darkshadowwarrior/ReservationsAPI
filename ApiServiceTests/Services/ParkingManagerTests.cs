b861f7c7e15f82d82febdfc729d315c3625efa3a
Ian Richards <cdeveloper@hotmail.co.uk>
2023-05-18 11:57:26 +0100

    Added parking manager and passing test

---
+++ ApiServiceTests/Services/ParkingManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using CarPark.Api.Services;
+
+namespace ApiServiceTests.Services
+{
+    public class ParkingManagerTests
+    {
+        [Fact]
+        public void GivenADateRange_ReturnsTrueIfSpacesAvailable()
+        {
+            var from = new DateTime(2023, 1, 1);
+            var to = new DateTime(2023, 1, 10);
+
+            var service = new ParkingManager();
+
+            var spacesAvailable = service.IsParkingAvailable(from, to);
+
+            Assert.False(spacesAvailable);
+        }
+    }
+}
---
+++ CarPark.Api/Services/ParkingManager.cs
@@ -0,0 +1,41 @@
+﻿namespace CarPark.Api.Services;
+
+public class ParkingManager
+{
+    private readonly Dictionary<DateTime, int> _reservations;
+
+    public ParkingManager()
+    {
+        _reservations = new Dictionary<DateTime, int>()
+        {
+            { new DateTime(2023, 1, 1), 7 },
+            { new DateTime(2023, 1, 2), 2 },
+            { new DateTime(2023, 1, 3), 0 },
+            { new DateTime(2023, 1, 4), 1 },
+            { new DateTime(2023, 1, 5), 3 },
+            { new DateTime(2023, 1, 6), 10 },
+            { new DateTime(2023, 1, 7), 10 },
+            { new DateTime(2023, 1, 8), 10 },
+            { new DateTime(2023, 1, 9), 10 },
+            { new DateTime(2023, 1, 10), 9 }
+        };
+    }
+
+    public bool IsParkingAvailable(DateTime from, DateTime to)
+    {
+        // Check if any of the dates within the range are already reserved
+        for (DateTime date = from; date <= to; date = date.AddDays(1))
+        {
+            if (_reservations.TryGetValue(date, out var reservation))
+            {
+                // A reservation exists for this date, check if all spaces are occupied
+                if (reservation >= 10)
+                {
+                    return false;  // All spaces are occupied
+                }
+            }
+        }
+
+        return true;  // Parking is available for all dates in the range
+    }
+}
