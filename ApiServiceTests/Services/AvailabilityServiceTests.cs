ba463d2aaf5f985cdb6b7ed044e74084cacb1cb2
Ian Richards <cdeveloper@hotmail.co.uk>
2023-05-18 11:28:33 +0100

    Added failing test for availability service

---
+++ ApiServiceTests/Services/AvailabilityServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using CarPark.Api.Models;
+
+namespace ApiServiceTests.Services
+{
+    public class AvailabilityServiceTests
+    {
+        [Fact]
+        public void GivenADateRange_ReturnsAvailableParkingSpaces()
+        {
+            var from = new DateTime(2023, 1, 1);
+            var to = new DateTime(2023, 12, 25);
+            var expectedSpaces = new List<ParkingSpace>();
+
+            var service = new AvailabilityService();
+
+            var spacesAvailable = service.GetAvailableParkingSpacesByDateRange(from, to);
+
+            Assert.Equal(spacesAvailable, expectedSpaces);
+        }
+    }
+}
---
+++ CarPark.Api/Services/AvailabilityService.cs
@@ -0,0 +1,11 @@
+﻿using CarPark.Api.Models;
+
+namespace ApiServiceTests.Services;
+
+public class AvailabilityService
+{
+    public IEnumerable<ParkingSpace> GetAvailableParkingSpacesByDateRange(DateTime from, DateTime to)
+    {
+        throw new NotImplementedException();
+    }
+}
